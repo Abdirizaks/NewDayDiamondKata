89061c54e51c93f9a9b97c48a8d93aa3664e7315
Abdirizak Jamal Ismail <abdirizak.j.Ismail@gmail.com>
2024-09-17 03:46:04 +0100

    Changed style of diamond and added test classes taking new implementation into account.

---
+++ NewDayDiamondKata/NewDayDiamondKata.UnitTest/CharacterCalculator.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NewDayDiamondKata.UnitTest;
+
+public class CharacterCalculatorTests
+{
+    private readonly CharacterCalculator _sut = new();
+
+    [Theory]
+    [InlineData('A', 'B')]
+    [InlineData('B', 'C')]
+    [InlineData('Y', 'Z')]
+    [InlineData('Z', ' ')]
+    public void IncrementCharacter_GivenInput_ShouldReturnExpectedResult(char input, char expected)
+    {
+        // Act
+        char result = _sut.IncrementCharacter(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData('C', 'A')]
+    [InlineData('D', 'B')]
+    [InlineData('Z', 'X')]
+    [InlineData('B', ' ')]
+    public void DecrementTwoCharactersBack_GivenInput_ShouldReturnExpectedResult(char input, char expected)
+    {
+        // Act
+        char result = _sut.DecrementTwoCharactersBack(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ NewDayDiamondKata/NewDayDiamondKata.UnitTest/DiamondCreatorTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NewDayDiamondKata.UnitTest;
+
+public class DiamondCreatorTests
+{
+    private readonly DiamondCreator _sut = new();
+
+    [Fact]
+    public void PrintForRowOneAndFive_ShouldReturnExpectedRow()
+    {
+        // Arrange
+        char inputChar = 'X';
+        string expectedRow = "  X";
+
+        // Act
+        string result = _sut.PrintForRowOneAndFive(inputChar);
+
+        // Assert
+        result.Should().Be(expectedRow);
+    }
+
+    [Fact]
+    public void PrintForRowTwoAndFour_ShouldReturnExpectedRow()
+    {
+        // Arrange
+        char inputChar = 'Y';
+        string expectedRow = " Y Y";
+        _sut.initialIndexCount = 4;
+        _sut.numberOfSpaces = 2;
+        _sut.eachRowIndexToSkip.Add(new KeyValuePair<int, int>(0, 2));
+
+        // Act
+        string result = _sut.PrintForRowTwoAndFour(inputChar);
+
+        // Assert
+        result.Should().Be(expectedRow);
+    }
+
+    [Fact]
+    public void PrintForRowThree_ShouldReturnExpectedRow()
+    {
+        // Arrange
+        char inputChar = 'Z';
+        string expectedRow = "Z   Z";
+        _sut.initialIndexCount = 5;
+        _sut.numberOfSpaces = 0;
+        _sut.eachRowIndexToSkip.Add(new KeyValuePair<int, int>(0, 2));
+        _sut.eachRowIndexToSkip.Add(new KeyValuePair<int, int>(1, 1));
+        _sut.eachRowIndexToSkip.Add(new KeyValuePair<int, int>(1, 3));
+
+        // Act
+        string result = _sut.PrintForRowThree(inputChar);
+
+        // Assert
+        result.Should().Be(expectedRow);
+    }
+}
